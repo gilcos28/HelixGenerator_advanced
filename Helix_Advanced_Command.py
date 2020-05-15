eb7e210e27511feaab6a97ded8a8c9a488351a75
gilcos28 <30881230+gilcos28@users.noreply.github.com>
2020-05-15 21:43:36 +1000

    Added features
    Added variable's to "start counting at"
    added an amended documentation

--- Helix_Advanced_Command.py
+++ Helix_Advanced_Command.py
@@ -34,10 +34,11 @@ def helix_point(radius, pitch, resolution, t, fnR, DT1, SP1, accumulator, fnZ, D
     try:
         radius = fnR(DT_temp, radius, resolution)
     except ZeroDivisionError:
-        message_string = message_string + "<br>WARNING: Zero division for radius at t=" + str(t*DT1) + "<br>   -> skip point calculation"
+        message_string = message_string + "<br>WARNING: Zero division for radius at [t]=" + str(t*DT1) + "<br>   -> skip point calculation"
         skip_ = True
-    if radius == None:
-        radius = 1      # when there's no equation in the equation tab then give it a value of 1 for user visualization
+        radius = 0   # nonsignificant value to return
+    # if radius == None:
+        # radius = 1      # when there's no equation in the equation tab then give it a value of 1 for user visualization
     x = radius * math.cos(2*math.pi*t/resolution)
     y = radius * math.sin(2*math.pi*t/resolution)
     # z = pitch * t / resolution
@@ -52,14 +53,14 @@ def helix_point(radius, pitch, resolution, t, fnR, DT1, SP1, accumulator, fnZ, D
             Z_previous[1] = Z_previous[1] * fnZ(DT_temp, pitch, resolution)  #z1
     else:
         # z = t * pitch / resolution
-        z = 0   # nonsignificant value to return
         try:
             z = fnZ(DT_temp,pitch, resolution)
         except ZeroDivisionError:
-            message_string = message_string + "<br>WARNING: Zero division for pitch at t=" + str(t*DT2) + "<br>   -> skip point calculation"
+            message_string = message_string + "<br>WARNING: Zero division for pitch at [t]=" + str(t*DT2) + "<br>   -> skip point calculation"
             skip_ = True
-        if z == None:    # when there's no equation in the equation tab then give it a value of 1 for user visualization
-            z = 1
+            z = 0   # nonsignificant value to return
+        # if z == None:    # when there's no equation in the equation tab then give it a value of 1 for user visualization
+            # z = 1
 
     # Create Fusion point
     point = adsk.core.Point3D.create(x, y, z)
@@ -91,23 +92,54 @@ def helix_maker(radius, revolutions, pitch, resolution, r1, DT1, SP1, accumulato
     # app = adsk.core.Application.get()
     # ui  = app.userInterface
     # ui.messageBox(z1)
+
     # prepare equation
     # r1_input is thhie object itself, review get_inputs(command_inputs) at parent class Fusion360CommandBase
     # good start for classes documentation: http://help.autodesk.com/view/fusion360/ENU/?guid=GUID-8B9041D5-75CC-4515-B4BB-4CF2CD5BC359
     preview = True
+    # fn = None
     try:
-        fnR = Expression(r1,["t", "r", "res"])
-        fnR(3, pitch, resolution)  # test if equation is legal, if not use previous points (points_old)
-        fnZ = Expression(z1,["t", "p", "res"])
-        fnZ(3, pitch, resolution)
+        fn = Expression(SP1, ["p", "r", "s", "n"])
+        SP1 = fn(pitch, radius, resolution, revolutions)
     except:
+        SP1 = None
+    if SP1 is None:
         preview = False
+        message_string = message_string + "<br>ERROR: radius starting point is incorrect"
+    # fn = None
+    try:
+        fn = Expression(SP2, ["p", "r", "s", "n"])
+        SP2 = fn(pitch, radius, resolution, revolutions)
+    except:
+        SP2 = None
+    if SP2 is None:
+        preview = False
+        message_string = message_string + "<br>ERROR: pitch starting point is incorrect"
+    try:
+        fnR = Expression(r1,["t", "r", "s"])
+        fn = fnR(3, pitch, resolution)  # test if equation is legal, if not use previous points (points_old)
+    except:
+        fn = None
+    if fn is None:
+        preview = False
+        message_string = message_string + "<br>ERROR: radius equation not legal"
+    try:
+        fnZ = Expression(z1,["t", "p", "s"])
+        fn = fnZ(3, pitch, resolution)
+    except:
+        fn = None
+    if fn is None:
+        preview = False
+        message_string = message_string + "<br>ERROR: pitch equation not legal"
+    if revolutions <= 0:
+        preview = False
+        message_string = message_string + "<br>ERROR: revolutions cannot be less than 0"
 
     if preview:
         # Iterate based on revolutions and resolution
         for t in range(0, int(revolutions*resolution)+1):
             # Add Point to collection
-            point, Z_previous, skip_, message_string = helix_point(radius, pitch, resolution, t, fnR, DT1, SP1, accumulator, fnZ, DT2, SP2, Z_previous, message_string)  #, ui_message)
+            point, Z_previous, skip_, message_string = helix_point(radius, pitch, resolution, t, fnR, DT1, SP1, accumulator, fnZ, DT2, SP2, Z_previous, message_string)
             if not skip_:
                 points.add(point)
 
@@ -189,11 +221,11 @@ class Helix_Advanced_Command(Fusion360CommandBase):
         selection_input.setSelectionLimits(1, 1)
 
         # Radius of the helix
-        radius_input = adsk.core.ValueInput.createByReal(25.54)
+        radius_input = adsk.core.ValueInput.createByReal(2.554)
         tabInputs1.addValueInput('radius', 'Radius', default_units, radius_input)
 
         # Pitch of the helix
-        pitch_input = adsk.core.ValueInput.createByReal(25.54)
+        pitch_input = adsk.core.ValueInput.createByReal(2.554)
         tabInputs1.addValueInput('pitch', 'Pitch', default_units, pitch_input)
 
         # Define points per revolution -> resolution
@@ -203,29 +235,31 @@ class Helix_Advanced_Command(Fusion360CommandBase):
         tabInputs1.addIntegerSpinnerCommandInput('revolutions', 'Revolutions', 0, 1000, 1, 5)
 
         # input radius Equation
-        tabInputs1.addStringValueInput ('r1', 'Radius Eq (t,r,res)', 'r') # r*(0.95^t)
+        tabInputs1.addStringValueInput ('r1', 'Radius Eq (t,r,s)', 'r') # r*(0.95^t)
 
         # Radius steps of Incrimination
         DT1_input = adsk.core.ValueInput.createByReal(1)
         tabInputs1.addValueInput('DT1', 'Delta Tr', '', DT1_input)
 
         # start counting from
-        start_r_input = adsk.core.ValueInput.createByReal(0)
-        tabInputs1.addValueInput('SP1', 'start counting at', '', start_r_input)
+        # start_r_input = adsk.core.ValueInput.createByReal(0)
+        # tabInputs1.addValueInput('SP1', 'start counting at', '', start_r_input)
+        tabInputs1.addStringValueInput('SP1', 'start counting at', '0')
 
         # accumulator algorithm for Z axis
         accumulator = tabInputs1.addBoolValueInput('accumulator', 'Accumulator on', True, '', False)
 
         # input height Equation
-        tabInputs1.addStringValueInput('z1', 'Pitch Eq (t,p,res)', 't*p/res')
+        tabInputs1.addStringValueInput('z1', 'Pitch Eq (t,p,s)', 't*p/s')
 
         # height steps of Incrimination
         DT2_input = adsk.core.ValueInput.createByReal(1)
         tabInputs1.addValueInput('DT2', 'Delta Tp', '', DT2_input)
 
         # start counting from
-        start_p_input = adsk.core.ValueInput.createByReal(0)
-        tabInputs1.addValueInput('SP2', 'start counting at', '', start_p_input)
+        # start_p_input = adsk.core.ValueInput.createByReal(0)
+        # tabInputs1.addValueInput('SP2', 'start counting at', '', start_p_input)
+        tabInputs1.addStringValueInput('SP2', 'start counting at', '0')
 
         # output window
         self.output_text = "Message Output:"
@@ -234,8 +268,8 @@ class Helix_Advanced_Command(Fusion360CommandBase):
         # instruction <em> <u> <p>
         Instruction = "<b><u>Instruction</u><br>"\
             + "<em><u>Variable’s:</u></em><br>"\
-            + "r -</b> radius, <b>p -</b> pitch, <b>res -</b> resolution<br>"\
-            + "<b>t</b> - steps value, (default value 0)<br>"\
+            + "r -</b> radius, <b>p -</b> pitch,<br> <b>s -</b> resolution, <b>n -</b> revolutions<br>"\
+            + "<b>t</b> - counter variable, (default value 0)<br>"\
             + "<b><em><u>Equation Input:</u></em></b><br>"\
             + "Utilize python “Equation Interpreter”<br>"\
             + "It can be: const,cos(x),sin(x),t^x,pi,e …<br>"\
@@ -244,16 +278,16 @@ class Helix_Advanced_Command(Fusion360CommandBase):
             + "x = radius *cos(2*pi*t/resolution)<br>"\
             + "y = radius *sin(2*pi*t/resolution)<br>"\
             + "<b>Pitch:</b> the equation is calculated directly. The result translates directly to Z axis.<br>"\
-            + "<b>t:</b> variable that count from 0 (by default) to Resolution*Revolutions+1.<br>"\
-            + "<b>Tr -</b> steps for t radius <br>"\
-            + "<b>Tp -</b> steps for t pitch <br>"\
-            + "<b>start counting at –</b> configure t starting point for counting<br>"\
+            + "<b>[t] -</b> variable that count from 0 (by default) to Resolution*Revolutions+1.<br>"\
+            + "<b>[Tr] -</b> steps for <b>[t]</b> radius <br>"\
+            + "<b>[Tp] -</b> steps for <b>[t]</b> pitch <br>"\
+            + "<b>start counting at –</b> configure <b>[t]</b> counter starting point (Accept variable’s [r,p,s,n])<br>"\
             + "<b><em><u>Accumulator (pitch only):</u></em></b><br>"\
             + "<em>[sum of previous results] +<br>"\
             + "([current result] * [pitch Eq])</em><br>"\
             + "Where [current results] initial value is pitch<br>"\
             + "<b><em><u>Message Output:</u></em></b><br>"\
-            + "Output warning messages for the user."
+            + "Output WARNING/ERROR messages"
 
         tabInputs2.addTextBoxCommandInput ('instruction', '', Instruction, 32, True)
 
